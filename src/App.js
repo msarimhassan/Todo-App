348fd75aa9ee4c4d6b550b02a5d0c4e369c815e0
sarim-dev <sarimhassan5@gmail.com>
2021-02-16 17:13:05 +0500

    React-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React,{useState} from "react";
 import './App.css';
 
 function App() {
+
+  const [inputText, setInputText]=useState("");
+  const [items,setItems]=useState([]);
+
+
+function handleChange(event){
+
+  const{value,name}=event.target;
+     console.log(name);
+
+     setInputText(value);
+}
+
+function handleClick(){
+      setItems((prevItems)=>{
+
+        return [...prevItems,inputText]
+      })
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <React.Fragment>
+      <div style={{backgroundColor:"#FC9902"}}>
+        <h1 style={{padding:"0px 0px 0px 40px"}}>TODO-APP</h1>
+      </div>
+  <div className="box">
+      <div className="app">
+      <h1 style={{backgroundColor:"#FC9902"}}>Todo App</h1>
+      <br/>
+      <input type="text" placeholder="Enter the task" name="task" onChange={handleChange}/>
+      <button onClick={handleClick} className="btn">Add</button>
+      <br/>
+      <br/>
+      <h4>Your Tasks</h4>
+      <br/>
+      {items.map((todoText)=>{
+        return <li>{todoText}</li>
+      })}
     </div>
+  </div>
+
+  </React.Fragment>
   );
 }
 
